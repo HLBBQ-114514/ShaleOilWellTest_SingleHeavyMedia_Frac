e13a59b29461882faff29563a58b65116abd1e6b
HLBQ-114514 <hlbbq@outlook.com>
2025-04-03 12:10:06 +0800

    #bug修改完成#程序正常运行

--- MathUnit.cs
+++ MathUnit.cs
@@ -33,10 +33,10 @@ namespace ShaleOilWellTest
             };
             Func<double, double> f2 = (xD) =>
             {
-                return SpecialFunctions.BesselI0(Sqrt(u / config.avgeta) * xD);
+                return SpecialFunctions.BesselI0( xD);
             };
-            double intg1 = 2* Sqrt(u / config.avgeta) * GaussLegendreRule.Integrate(f1, 1e-4, Sqrt(u / config.avgeta), 32);//+ GaussLegendreRule.Integrate(f1, 1e-2, 1, 32);
-            double intg2 = 2 * GaussLegendreRule.Integrate(f2, 0, 1, 32);
+            double intg1 = 2/ Sqrt(u / config.avgeta) * GaussLegendreRule.Integrate(f1, 1e-4, Sqrt(u / config.avgeta), 32);//+ GaussLegendreRule.Integrate(f1, 1e-2, 1, 32);
+            double intg2 = 2 * Sqrt(u / config.avgeta) * GaussLegendreRule.Integrate(f2, 1e-4, Sqrt(u / config.avgeta), 32);
 
             double omega = config.omega;// config.phi * config.ct * config.h / (config.avgphiCt * config.h_t);//0.5;//
             double zeta = config.zeta;//(config.k / config.mu) * config.h / (config.avgLambda * config.h_t);//1; //
@@ -244,5 +244,151 @@ namespace ShaleOilWellTest
 
         }
 
+        /// <summary>
+        /// 变形第一类整数阶贝塞尔函数I
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double TransformativeIntegerBessel1stFunction(int n, double x)
+        {
+            int i, m;
+            double t, y, p = 0, b0, b1, q;
+            double[] a ={ 1.0,3.5156229,3.0899424,1.2067492,
+                                 0.2659732,0.0360768,0.0045813};
+            double[] b ={ 0.5,0.87890594,0.51498869,
+                      0.15084934,0.02658773,0.00301532,0.00032411};
+            double[] c ={ 0.39894228,0.01328592,0.00225319,
+                                -0.00157565,0.00916281,-0.02057706,
+                                 0.02635537,-0.01647633,0.00392377};
+            double[] d ={ 0.39894228,-0.03988024,-0.00362018,
+                                0.00163801,-0.01031555,0.02282967,
+                                -0.02895312,0.01787654,-0.00420059};
+            if (n < 0) n = -n;
+            t = Math.Abs(x);
+            if (n != 1)
+            {
+                if (t < 3.75)
+                {
+                    y = (x / 3.75) * (x / 3.75);
+                    p = a[6];
+                    for (i = 5; i >= 0; i--) p = p * y + a[i];
+                }
+                else
+                {
+                    y = 3.75 / t;
+                    p = c[8];
+                    for (i = 7; i >= 0; i--) p = p * y + c[i];
+                    p = p * Math.Exp(t) / Math.Sqrt(t);
+                }
+            }
+            if (n == 0) return (p);
+            q = p;
+            if (t < 3.75)
+            {
+                y = (x / 3.75) * (x / 3.75);
+                p = b[6];
+                for (i = 5; i >= 0; i--) p = p * y + b[i];
+                p = p * t;
+            }
+            else
+            {
+                y = 3.75 / t;
+                p = d[8];
+                for (i = 7; i >= 0; i--) p = p * y + d[i];
+                p = p * Math.Exp(t) / Math.Sqrt(t);
+            }
+            if (x < 0.0) p = -p;
+            if (n == 1) return (p);
+            if (x == 0.0) return (0.0);
+            y = 2.0 / t;
+            t = b0 = 0.0;
+            b1 = 1.0;
+            m = n + (int)Math.Sqrt(40.0 * n);
+            m = 2 * m;
+            for (i = m; i > 0; i--)
+            {
+                p = b0 + i * y * b1;
+                b0 = b1;
+                b1 = p;
+                if (Math.Abs(b1) > 1.0e+10)
+                {
+                    t = t * 1.0e-10;
+                    b0 = b0 * 1.0e-10;
+                    b1 = b1 * 1.0e-10;
+                }
+                if (i == n) t = b0;
+            }
+            p = t * q / b1;
+            if ((x < 0.0) && (n % 2 == 1)) p = -p;
+            return (p);
+        }
+
+        /// <summary>
+        /// 变形第二类整数阶贝塞尔函数K
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static double TransformativeIntegerBessel2ndFunction(int n, double x)
+        {
+            int i;
+            double y, p = 0, b0, b1;
+
+            double[] a ={ -0.57721566,0.4227842,0.23069756,
+           0.0348859,0.00262698,0.0001075,0.0000074};
+            double[] b ={ 1.0,0.15443144,-0.67278579,
+           -0.18156897,-0.01919402,-0.00110404,-0.00004686};
+            double[] c ={ 1.25331414,-0.07832358,0.02189568,
+           -0.01062446,0.00587872,-0.0025154,0.00053208};
+            double[] d ={ 1.25331414,0.23498619,-0.0365562,
+           0.01504268,-0.00780353,0.00325614,-0.00068245};
+            if (n < 0) n = -n;
+            if (x < 0.0) x = -x;
+            if (x == 0.0) return (1.0e+70);
+            if (n != 1)
+            {
+                if (x < 2.0 || x == 2.0)
+                {
+                    y = x * x / 4.0; p = a[6];
+                    for (i = 5; i >= 0; i--) p = p * y + a[i];
+                    p = p - TransformativeIntegerBessel1stFunction(0, x) * Math.Log(x / 2.0);
+                }
+                else
+                {
+                    y = 2.0 / x; p = c[6];
+                    for (i = 5; i >= 0; i--) p = p * y + c[i];
+                    p = p * Math.Exp(-x) / Math.Sqrt(x);
+                }
+            }
+            if (n == 0) return (p);
+            b0 = p;
+            if (x < 2.0 || x == 2.0)
+            {
+                y = x * x / 4.0;
+                p = b[6];
+                for (i = 5; i >= 0; i--) p = p * y + b[i];
+                p = p / x + TransformativeIntegerBessel1stFunction(1, x) * Math.Log(x / 2.0);
+            }
+            else
+            {
+                y = 2.0 / x;
+                p = d[6];
+                for (i = 5; i >= 0; i--) p = p * y + d[i];
+                p = p * Math.Exp(-x) / Math.Sqrt(x);
+            }
+            if (n == 1) return (p);
+            b1 = p;
+            y = 2.0 / x;
+            for (i = 1; i < n; i++)
+            {
+                p = b0 + i * y * b1;
+                b0 = b1;
+                b1 = p;
+            }
+            return (p);
+        }
+
     }
 }
+
