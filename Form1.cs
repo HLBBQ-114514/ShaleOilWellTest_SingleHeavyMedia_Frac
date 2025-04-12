906294cbcabb302fd5a5236dddb40775e76c0fd4
HLBQ-114514 <hlbbq@outlook.com>
2025-04-12 12:55:30 +0800

    #加上了流量Q

--- Form1.cs
+++ Form1.cs
@@ -25,18 +25,18 @@ namespace ShaleOilWellTest
                 }
             }
 
-            ReservoirConfigDoubleMedia config1 = new ReservoirConfigDoubleMedia(h:2, k:0.00001, Bo:1.2, mu:15, ctm: 0.3e-4, ctf: 3e-4,Cs:0.01, rw:0.12, re:20000,  phiM:0.06,phiF:0.01, s:0.4, sf:0.1, xf:100)
+            ReservoirConfigDoubleMedia config1 = new ReservoirConfigDoubleMedia(h:4, k:0.00001, Bo:1.2, mu:15, ctm: 0.3e-4, ctf: 3e-4,Cs:0.01, rw:0.12, re:2000,  phiM:0.06,phiF:0.01, s:0.4, sf:0.1, xf:100)
             {
                 Pi = 32
             };
-            ReservoirConfigDoubleMedia config2 = new ReservoirConfigDoubleMedia(h: 4, k: 0.0002, 1.2, 12, ctm: 0.5e-4, ctf: 3e-4, Cs: 0.01, 0.12, 20000,  phiM: 0.1, phiF: 0.014, s:0.4, sf:0.1, xf:100)
+            ReservoirConfigDoubleMedia config2 = new ReservoirConfigDoubleMedia(h: 4, k: 0.0002, 1.2, 12, ctm: 0.5e-4, ctf: 3e-4, Cs: 0.01, 0.12, 2000,  phiM: 0.1, phiF: 0.014, s:0.4, sf:0.1, xf:100)
             {
                 Pi = 32
             }
             ;//4, 0.0002, 2.5618E-4, 12, 5e-4, 1.4, 0.1, 0.12, 1.5
             
-            config1.factorlessness(config2, omega: 0.7, lambdaF: 1e-4,zeta:0.7);
-            config2.factorlessness(config1, omega: 0.3, lambdaF: 1e-4, zeta:0.3);
+            config1.factorlessness(config2, omega: 0.7, lambdaF: 1e-2,zeta:0.7);
+            config2.factorlessness(config1, omega: 0.3, lambdaF: 1e-2, zeta:0.3);
             configs[0] = config1;
             configs[1] = config2;
 
@@ -55,7 +55,7 @@ namespace ShaleOilWellTest
                 }
                 //Console.WriteLine(i);
                 //textBox7.Text += Math.Pow(10, 0.1 * i) + "\t" + Pwf + "\t" + DPwf + "\t" + Qwf + "\t" + DQwf + "\r\n";//
-                textBox1.Text += Math.Pow(10, 0.1 * i) + "\t" + Pwf + "\t" + DPwf+"\t" + "\r\n";//+ qwf[0] + "\t" + qwf[1]
+                textBox1.Text += Math.Pow(10, 0.1 * i) + "\t" + Pwf + "\t" + DPwf+"\t" + qwf[0] + "\t" + qwf[1] + "\r\n";//
 
             }
         }
